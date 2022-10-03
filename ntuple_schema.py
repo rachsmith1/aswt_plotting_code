05ef40389bd708e4c36bc3084df580382fc607bc
Smith <recsmith@sdf-login01.slac.stanford.edu>
2022-10-02 22:11:53 -0700

    new plotting code

--- ntuple_schema.py
+++ ntuple_schema.py
@@ -1,7 +1,7 @@
 from coffea.nanoevents.schemas.base import BaseSchema, zip_forms
 from coffea.nanoevents.methods import base, vector
 
-class NtupleSchema(BaseSchema):
+class ZpeakSchema(BaseSchema):
     def __init__(self, base_form):
         super().__init__(base_form)
         self._form["contents"] = self._build_collections(self._form["contents"])
@@ -16,10 +16,134 @@ class NtupleSchema(BaseSchema):
         el_mass   = branch_forms["el_m"]
         el_charge = branch_forms["el_charge"]
         content = {"pt": el_pt, "eta": el_eta, "phi": el_phi, "mass": el_mass, "charge": el_charge}
-        output["Electron"] = zip_forms(content, "Electron", 'PtEtaPhiMLorentzVector')        
+        output["Electron"] = zip_forms(content, "Electron", 'PtEtaPhiMLorentzVector')       
         
         return output
         
+    @property
+    def behavior(self):
+        behavior = {}
+        behavior.update(base.behavior)
+        behavior.update(vector.behavior)
+        return behavior
+    
+class DiphotonSchema(BaseSchema):
+    def __init__(self, base_form):
+        super().__init__(base_form)
+        self._form["contents"] = self._build_collections(self._form["contents"])
+        
+    def _build_collections(self, branch_forms):
+        
+        output = {}
+        
+        ph_pt     = branch_forms["ph_pt_NOSYS"]
+        ph_eta    = branch_forms["ph_eta"]
+        ph_phi    = branch_forms["ph_phi"]
+        ph_mass   = branch_forms["ph_m"]
+        content = {"pt": ph_pt, "eta": ph_eta, "phi": ph_phi, "mass": ph_mass}
+        output["Photon"] = zip_forms(content, "Photon", 'PtEtaPhiMLorentzVector')   
+        
+        #output["el_eta"] = branch_forms["el_eta"]
+        #output["mu_eta"] = branch_forms["mu_eta"]
+        
+        return output
+        
+    @property
+    def behavior(self):
+        behavior = {}
+        behavior.update(base.behavior)
+        behavior.update(vector.behavior)
+        return behavior
+    
+class NtupleSchema(BaseSchema):
+    def __init__(self, base_form):
+        super().__init__(base_form)
+        self._form["contents"] = self._build_collections(self._form["contents"])
+
+    def _build_collections(self, branch_forms):
+
+        output = {}
+        objects = {
+            "Electron": {
+                'shortname': "el" ,
+                'vars': [ 'pt' , 'eta' , 'phi' , 'charge' , 'select_loose' , 'mass' ] , 
+            } ,
+            "Muon": {
+                'shortname': "mu" ,
+                'vars': [ 'pt' , 'eta' , 'phi' , 'charge' , 'select_medium' , 'select_tight' , 'mass' ] ,
+            } ,
+            "Jet": {
+                'shortname': "jet" ,
+                'vars': [ 'pt' , 'eta' , 'phi' , 'mass' ] ,
+            } ,
+            "MET": {
+                'shortname': "met" ,
+                'vars': [ 'sumet' , 'name' ] ,
+            }
+        }
+
+        for (obj,objname) in zip(objects.items(),objects):
+            objname = obj[1]["shortname"]
+            content = {}
+            for var in obj[1]['vars']:
+                branchname = f'{objname}_{var}'
+                if( var in ['pt','sumet','name','select_tight','select_medium','select_loose'] ):
+                    branchname = f'{branchname}_NOSYS'
+                elif( var == 'mass' ): branchname = branchname.replace( 'mass' , 'm' )
+                content[var] = branch_forms[branchname]
+            output[obj[0]] = zip_forms(content, obj[0], 'PtEtaPhiMLorentzVector')
+        
+        output["weight"] = branch_forms["generatorWeight_NOSYS"]
+
+        return output
+
+    @property
+    def behavior(self):
+        behavior = {}
+        behavior.update(base.behavior)
+        behavior.update(vector.behavior)
+        return behavior
+    
+class NtupleSchemaData(BaseSchema):
+    def __init__(self, base_form):
+        super().__init__(base_form)
+        self._form["contents"] = self._build_collections(self._form["contents"])
+
+    def _build_collections(self, branch_forms):
+
+        output = {}
+        objects = {
+            "Electron": {
+                'shortname': "el" ,
+                'vars': [ 'pt' , 'eta' , 'phi' , 'charge' , 'select_loose' , 'mass' ] , 
+            } ,
+            "Muon": {
+                'shortname': "mu" ,
+                'vars': [ 'pt' , 'eta' , 'phi' , 'charge' , 'select_medium' , 'select_tight' , 'mass' ] ,
+            } ,
+            "Jet": {
+                'shortname': "jet" ,
+                'vars': [ 'pt' , 'eta' , 'phi' , 'mass' ] ,
+            } ,
+            "MET": {
+                'shortname': "met" ,
+                'vars': [ 'sumet' , 'name' ] ,
+            }
+        }
+
+        for (obj,objname) in zip(objects.items(),objects):
+            objname = obj[1]["shortname"]
+            content = {}
+            for var in obj[1]['vars']:
+                branchname = f'{objname}_{var}'
+                if( var in ['pt','sumet','name','select_tight','select_medium','select_loose'] ):
+                    branchname = f'{branchname}_NOSYS'
+                elif( var == 'mass' ): branchname = branchname.replace( 'mass' , 'm' )
+                content[var] = branch_forms[branchname]
+            output[obj[0]] = zip_forms(content, obj[0], 'PtEtaPhiMLorentzVector')
+
+        return output
+
     @property
     def behavior(self):
         behavior = {}
